76edf008e7f0e8ee5a6befd1334223af187b0d23
achyutpsd <achyutpsd@gmail.com>
2020-04-18 20:22:17 +0800

    weatherapp

---
+++ WeatherApp/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!D:\Achyut\Learning\Course\BotProjects\Gypsybotprojects\WeatherApp\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
